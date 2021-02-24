62542fced7b157c97018a525e9b71f0b419c8800
yummydum <atsushisumita0421@gmail.com>
2021-02-24 23:02:06 +0900

    make the example even simpler

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,3 @@
-FROM jupyter/minimal-notebook:016833b15ceb
-USER root
-RUN pip install jupyter_contrib_nbextensions papermill seaborn
-RUN jupyter contrib nbextension install --user
-RUN jupyter nbextension enable toc2/main --user
+FROM jupyter/base-notebook
+RUN pip install papermill seaborn
 ENTRYPOINT ["python","run_notebook.py"]
--- run_notebook.py
+++ run_notebook.py
@@ -1,15 +1,10 @@
-"""
-Usage:
-    python run_notebook <color> (G,E,F,H,D,I,J)
-"""
-
 import sys
 import papermill as pm
 
 # Check if the parameter is properly set
 nb_path = 'notebooks/example.ipynb'
-result = pm.inspect_notebook(nb_path)
-assert 'color' in result
+params = pm.inspect_notebook(nb_path)
+assert 'color' in params
 
 # Execute the notebook with the parameter
 color = sys.argv[1]
