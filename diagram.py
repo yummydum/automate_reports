a17f80818a822368208e3bd7fa085754da0ced70
yummydum <atsushisumita0421@gmail.com>
2021-02-25 10:48:03 +0900

    refine diagram and add pptx

--- diagram.py
+++ diagram.py
@@ -4,8 +4,15 @@ from diagrams.aws.storage import SimpleStorageServiceS3,EFS
 from diagrams.onprem.client import Users
 from diagrams.onprem.container import Docker
 from diagrams.onprem.workflow import Airflow
+from diagrams.custom import Custom
 
 with Diagram("Report Generation"):
+    users = Users("Audience")
+    commuter = Custom("Commuter","commuter.png")
+
+    with Cluster("Adhoc flow"):
+        adhoc_flow =  Users("Data Scientists") >> EC2("Adhoc workstation") >> Edge(
+                label="Mount") >> EFS("EFS") >> commuter 
 
     with Cluster("workers"):
         with Cluster("worker1"):
@@ -14,12 +21,9 @@ with Diagram("Report Generation"):
             worker2 = Docker("Jupyter+Papermill") - EC2("Agent2")
         with Cluster("worker3"):
             worker3 = Docker("Jupyter+Papermill") - EC2("Agent3")
+        workers = [worker1, worker2, worker3]
 
-    workers = [worker1, worker2, worker3]
+    scheduled_flow = Airflow("Airflow") >> Edge(
+        label="Schedule notebook execution") >> Batch("AWS Batch") >> workers >> SimpleStorageServiceS3("S3") >> commuter
 
-    Airflow("Airflow") >> Edge(
-        label="Schedule notebook execution") >> Batch("AWS Batch") >> workers >> SimpleStorageServiceS3("S3") >> Edge(
-        label="Commuter") >> Users("End users")
-
-    Users("Data analysts") >> EC2("Adhoc workstation") >> Edge(
-        label="Mount") >> EFS("EFS") >> Edge(label="Commuter") >> Users("End users")
+    commuter >> users
