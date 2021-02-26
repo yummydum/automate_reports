8f258d2324afe09c8d067fa45e7dda0aa5e54f62
yummydum <atsushisumita0421@gmail.com>
2021-02-26 10:54:36 +0900

    finalize slide

--- diagram.py
+++ diagram.py
@@ -8,22 +8,25 @@ from diagrams.custom import Custom
 
 with Diagram("Report Generation"):
     users = Users("Audience")
-    commuter = Custom("Commuter","commuter.png")
+
+    with Cluster("Commuter"):
+        commuter = Custom("Commuter","commuter.png") - EC2("Web server")
 
     with Cluster("Adhoc flow"):
         adhoc_flow =  Users("Data Scientists") >> EC2("Adhoc workstation") >> Edge(
                 label="Mount") >> EFS("EFS") >> commuter 
 
-    with Cluster("workers"):
-        with Cluster("worker1"):
-            worker1 = Docker("Jupyter+Papermill") - EC2("Agent1")
-        with Cluster("worker2"):
-            worker2 = Docker("Jupyter+Papermill") - EC2("Agent2")
-        with Cluster("worker3"):
-            worker3 = Docker("Jupyter+Papermill") - EC2("Agent3")
-        workers = [worker1, worker2, worker3]
+    with Cluster("Scheduled flow"):
+        with Cluster("workers"):
+            with Cluster("worker1"):
+                worker1 = Docker("Jupyter+Papermill") - EC2("Agent1")
+            with Cluster("worker2"):
+                worker2 = Docker("Jupyter+Papermill") - EC2("Agent2")
+            with Cluster("worker3"):
+                worker3 = Docker("Jupyter+Papermill") - EC2("Agent3")
+            workers = [worker1, worker2, worker3]
 
-    scheduled_flow = Airflow("Airflow") >> Edge(
-        label="Schedule notebook execution") >> Batch("AWS Batch") >> workers >> SimpleStorageServiceS3("S3") >> commuter
+        scheduled_flow = Airflow("Airflow") >> Edge(
+            label="Schedule notebook execution") >> Batch("AWS Batch") >> workers >> SimpleStorageServiceS3("S3") >> commuter
 
     commuter >> users
